77a4370d9cc2458db734104d1da6051353c843dc
AlexIchenskiy <aichenskiy@gmail.com>
2020-08-30 18:30:42 +0200

    Updates

---
+++ precache-manifest.aa174490528d9c7c766ad80ddb2b3061.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ac196b6f76611a129862f0ead67d12",
+    "url": "/credit-card-react/index.html"
+  },
+  {
+    "revision": "17aa73cc329c2092fd36",
+    "url": "/credit-card-react/static/css/main.c760e642.chunk.css"
+  },
+  {
+    "revision": "b2529a2766471e07ea9d",
+    "url": "/credit-card-react/static/js/2.56ec3ad2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/credit-card-react/static/js/2.56ec3ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17aa73cc329c2092fd36",
+    "url": "/credit-card-react/static/js/main.3f6a3d2d.chunk.js"
+  },
+  {
+    "revision": "77c6914127c6e5dd3973",
+    "url": "/credit-card-react/static/js/runtime-main.fd1e49d7.js"
+  },
+  {
+    "revision": "730fad8fe05209d63c02f14fd334afaf",
+    "url": "/credit-card-react/static/media/cardchip.730fad8f.png"
+  },
+  {
+    "revision": "df5fcfcb03ac918e8a6222c0e32ed3ea",
+    "url": "/credit-card-react/static/media/visalogo.df5fcfcb.png"
+  }
+]);
